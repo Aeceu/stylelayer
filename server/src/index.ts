d2a949a75ce6112879ea9ef5b400887c87d58bbc
kaneki081 <joseacebuche2@gmail.com>
2024-07-28 18:00:14 +0800

    changes variables for production 9

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,36 +11,21 @@ import product from "./routes/product";
 
 dotenv.config();
 const app = express();
-
 const allowedOrigin = [
   "https://stylelayer.vercel.app",
   "https://admin.stylelayer.vercel.app",
   "http://localhost:5173",
 ];
-app.use((req: Request, res: Response, next: NextFunction) => {
-  const origin = req.headers.origin;
-  if (allowedOrigin.includes(origin!)) {
-    res.header("Access-Control-Allow-Credentials", "true");
-  }
-
-  next();
-});
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (allowedOrigin.indexOf(origin!) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    optionsSuccessStatus: 200,
-  })
-);
 
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: allowedOrigin,
+    credentials: true,
+  })
+);
 
 app.use("/api/v1", user);
 app.use("/api/v1", cart);
