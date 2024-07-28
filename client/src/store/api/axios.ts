f4bfc7104e769e28d2f7ca6b85f23ed3a98659d4
kaneki081 <joseacebuche2@gmail.com>
2024-07-28 17:52:18 +0800

    changes variables for production 8

--- client/src/store/api/axios.ts
+++ client/src/store/api/axios.ts
@@ -1,4 +1,8 @@
 import axios from "axios";
+
+const production_url = "https://sstylelayer-h7gn.vercel.app";
+// const local_url = "http://localhost:4200";
+
 export default axios.create({
-  baseURL: "https://serverstylelayer-kaneki081s-projects.vercel.app/api/v1",
+  baseURL: `${production_url}/api/v1`,
 });
